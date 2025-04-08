d4b3eea515f424cd041e3cb05fa0ababa9e1d17d
Romigo97 <roma.kolyada939@gmail.com>
2025-04-08 22:21:22 +0300

    add model for images

--- places/models.py
+++ places/models.py
@@ -15,7 +15,36 @@ class Place(models.Model):
         verbose_name='Развернутое описание'
     )
     lng = models.FloatField(verbose_name='Долгота')
-    lan = models.FloatField(verbose_name='Широта')
+    lat = models.FloatField(verbose_name='Широта')
+
+    first_image = models.ForeignKey(
+        'Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='places_with_first_image',
+        verbose_name='Первая картинка'
+    )
+
+    second_image = models.ManyToManyField(
+        'Image',
+        blank=True,
+        related_name='places_with_second_image',
+        verbose_name='Вторая картинка'
+    )
     
+    def __str__(self):
+        return self.title
+    
+class Image(models.Model):
+    TYPE_IMAGE = [
+        ('first_image', 'Первая картинка'),
+        ('second_image', 'Вторая картинка')
+    ]
+
+    title = models.CharField(max_length=100, verbose_name='Название картинки')
+    type_image = models.CharField(max_length=50, choices=TYPE_IMAGE, verbose_name='Тип картинки')
+    image = models.ImageField(verbose_name='Картинка')
+
     def __str__(self):
         return self.title
