699805730c5b55269a6470a205b553a837adf73e
Romigo97 <roma.kolyada939@gmail.com>
2025-04-09 14:08:16 +0300

    move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+from django.http import JsonResponse
+from .models import Place
+import json
+import os
 
-# Create your views here.
+
+def load_json():
+    places = []
+    static_dir = r'/Users/roma.kolyadayandex.ru/Desktop/where_to_go/static/places'
+
+    for filename in os.listdir(static_dir):
+        if filename.endswith('.json'):
+            filepath = os.path.join(static_dir, filename)
+
+            with open(filepath, 'r', encoding='utf-8') as file:
+                place_data = json.load(file)
+                place_data['placeId'] = os.path.splitext(filename)[0]
+                places.append(place_data)
+
+    return places
+
+
+def index(request):
+    places = load_json()
+    features = []
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place["coordinates"]["lng"], place["coordinates"]["lat"]]
+            },
+            "properties": {
+                "title": place["title"],
+                "placeId": place["placeId"],
+                "detailsUrl": static(f'places/{place["placeId"]}.json')
+            }
+        }
+        features.append(feature)
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    geojson_str = json.dumps(geojson, ensure_ascii=False, indent=4)
+    return render(request, 'index.html', {'geojson': geojson_str})
