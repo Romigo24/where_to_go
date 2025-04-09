e0a976a994bf83e4b8485c03f660d015615bc134
Romigo97 <roma.kolyada939@gmail.com>
2025-04-09 17:31:59 +0300

    Update admin.py

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 from .models import Place, Image
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
+ 
 
-
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 1
     readonly_fields = ('image_preview', )
@@ -17,7 +18,7 @@ class ImageInline(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ('title',)
     inlines = [ImageInline]
 
