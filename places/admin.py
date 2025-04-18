871badbe19b247aa3570e57a556299347eefb8e0
Romigo97 <roma.kolyada939@gmail.com>
2025-04-18 22:35:45 +0300

    specified the maximum width of the preview image

--- places/admin.py
+++ places/admin.py
@@ -11,7 +11,7 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     readonly_fields = ('image_preview', )
 
     def image_preview(self, obj):
-        return format_html('<img src="{}" style="max-height: 200px; width: auto;" />', obj.image.url)
+        return format_html('<img src="{}" style="max-height: 200px; max-width: 150;" />', obj.image.url)
     
     image_preview.short_description = 'Превью'
 
